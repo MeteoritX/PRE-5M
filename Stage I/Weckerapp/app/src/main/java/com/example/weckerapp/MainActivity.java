7c85f13ca5be9ac32a6ddd690d745be8dc22552e
MistlbergerR190131 <Ralph.mi@gmx.at>
2023-10-22 13:02:25 +0200

    Grafikbeständigkeit und Wecker-Layout

--- Stage I/Weckerapp/app/src/main/java/com/example/weckerapp/MainActivity.java
+++ Stage I/Weckerapp/app/src/main/java/com/example/weckerapp/MainActivity.java
@@ -1,15 +1,20 @@
 package com.example.weckerapp;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.media.MediaPlayer;
 import android.media.Ringtone;
 import android.media.RingtoneManager;
 import android.os.Build;
 import android.os.SystemClock;
 import android.provider.Settings;
+import android.support.annotation.NonNull;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TabHost;
 import android.widget.TextClock;
@@ -47,10 +52,34 @@ public class MainActivity extends AppCompatActivity {
         spec.setIndicator("Stoppuhr");
         tabHost.addTab(spec);
 
-
-
-
-
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //Setting up a top menue for settings / access to defined alarms
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.top_menue, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        //Click listener: Checking which menue item has been clicked
+        switch (item.getItemId()){
+            case R.id.settings:
+                Toast.makeText(this, "settings clicked", Toast.LENGTH_SHORT).show();
+                return true;
+
+            case R.id.alarms:
+                Toast.makeText(this, "alarms clicked", Toast.LENGTH_SHORT).show();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void button_setAlarm_Clicked(View view) {
+
+    }
 }
