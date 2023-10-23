fcffcbd480d8a60b4f73ec83b1f7e9f1bc4aa635
MistlbergerR190131 <Ralph.mi@gmx.at>
2023-10-23 16:57:59 +0200

    Settings Activity aufgesetzt

--- Stage I/Weckerapp/app/src/main/java/com/example/weckerapp/SettingsActivity.java
+++ Stage I/Weckerapp/app/src/main/java/com/example/weckerapp/SettingsActivity.java
@@ -1,15 +1,24 @@
 package com.example.weckerapp;
 
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 
 public class SettingsActivity extends AppCompatActivity {
 
+    Toolbar toolbar_settings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all_settings);
+
+        toolbar_settings = findViewById(R.id.toolbar_settings);
+        setSupportActionBar(toolbar_settings);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setTitle(getResources().getString(R.string.text_settings_title));
     }
 }
